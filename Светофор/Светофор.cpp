c4e2a8c114426e718bb3ce566df4df5e7b745508
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-18 06:04:07 +0300

    Добавьте файлы проекта.

---
+++ Светофор/Светофор.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace  std;
+int main()
+{
+    cout << "The traffic light!" << endl;
+    int Thetrafficlight;//переменная
+    cin >> Thetrafficlight;
+    if (Thetrafficlight < 0)//если меньше нуля
+    {
+        cout << "GO" << endl;
+    }
+
+    else//иначе
+    {
+        cout << "Stop" << endl;
+    }
+    /* code */
+    return 0;
+}
+
